04bd29e9183e401df8f9a9d81d067d4540f5754e
MonkFallsInSnow <lewinc@oregonstate.edu>
2017-01-19 12:11:40 -0500

    testing pylatex

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import logging
 from flask import Flask, render_template
+from pylatex import Document, Section, Subsection, Command
+from pylatex.utils import italic, NoEscape
+
 app = Flask(__name__)
 
+def fill_document(doc):
+    """Add a section, a subsection and some text to the document.
 
+    :param doc: the document
+    :type doc: :class:`pylatex.document.Document` instance
+    """
+    with doc.create(Section('A section')):
+        doc.append('Some regular text and some ')
+        doc.append(italic('italic text. '))
+
+        with doc.create(Subsection('A subsection')):
+            doc.append('Also some crazy characters: $&#{}')
+			
 @app.route('/')
 def renderIndex():
+	doc = Document('basic')
+	fill_document(doc)
+
+	doc.generate_pdf(clean_tex=False)
 	return render_template('index.html')
 
 @app.errorhandler(500)
