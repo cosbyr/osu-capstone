5d3ea01840537f68715ce68239661d8c2e2bdf3d
MonkFallsInSnow <lewinc@oregonstate.edu>
2017-01-19 00:25:38 -0500

    sync

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('osu-capstone.herokuapp.com')
+@app.route('/')
 def renderIndex():
 	return render_template('index.html')
 
