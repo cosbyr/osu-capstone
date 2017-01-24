032f63253907fc56c7f770739758446a3c6bdff4
MonkFallsInSnow <lewinc@oregonstate.edu>
2017-01-24 17:48:20 -0500

    updated requirements and imports to reflecx that we will not be using the python latex library

--- handlers/texHandler.py
+++ handlers/texHandler.py
@@ -1,7 +1,5 @@
 from __future__ import print_function #debug
 import os,sys #debug
-#from latex import build_pdf, LatexBuildError
-import latex
 
 def genAward(texFile):
 	os.system('pdflatex ' + texFile)
