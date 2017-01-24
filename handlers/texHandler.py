0825cc12e17ca80f5597fb1391c55b9bc818f77d
MonkFallsInSnow <lewinc@oregonstate.edu>
2017-01-24 11:38:55 -0500

    cannot produce pdf with image on local server. testing pdf generation on heroku server to see if the buildpack produces a different result than the local latex compiler

--- handlers/texHandler.py
+++ handlers/texHandler.py
@@ -1,8 +1,20 @@
-from latex import build_pdf
+from __future__ import print_function #debug
+import sys #debug
+from latex import build_pdf, LatexBuildError
 
 def genAward(tex,filename):
 	filename = './' + filename + '.pdf'
 	pdf = build_pdf(tex)
 	pdf.save_to(filename)
 	return filename
+	'''try:
+		pdf = build_pdf(tex)
+		pdf.save_to(filename)
+		return filename
+	except LatexBuildError as e:
+		for err in e.get_errors():
+			print('Error in {0[filename]}, line {0[line]}: {0[error]}'.format(err),file=sys.stderr)
+			print('    {}'.format(err['context'][1]),file=sys.stderr)
+			print()'''
+			
 	
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from __future__ import print_function #debug
 import sys,os #debug
 import logging
 from handlers import texHandler, dbHandler
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, abort
 
 db = dbHandler.connectDB()
 app = Flask(__name__)
@@ -14,12 +14,17 @@ def renderIndex():
 @app.route('/latex')
 def renderPDF():
 	#just a test. changes will be made to this when we have a better idea how this function should... uh, function
-	tex = (r"\documentclass{article}"
+	'''tex = (r"\documentclass{article}"
 			 r"\begin{document}"
 			 r"Hello, world!"
 			 r"\end{document}")
+	
 	filename = 'test'
-	pdf = texHandler.genAward(tex,filename)
+	pdf = texHandler.genAward(tex,filename)'''
+	
+	with open('./testaward2.tex','r') as file:
+		pdf = texHandler.genAward(file,'test2')
+	
 	return send_file(pdf)
 
 
