93e405ab42b385e64afcb539cd5bafc2d7199710
Ryan Cosby <rcosby@jub.com>
2017-01-31 13:12:19 -0700

    signature image now saved in S3 using email for the name

--- static/js/uploadSig.js
+++ static/js/uploadSig.js
@@ -30,9 +30,14 @@
       request.
     */
     function getSignedRequest(file){
-      const xhr = new XMLHttpRequest();
-      xhr.open("GET", "/sign_s3?file_name="+file.name+"&file_type="+file.type);
-      xhr.onreadystatechange = () => {
+		var email;
+		email = document.getElementById('email').value;
+		email = email.replace("@","_");
+		email = email.replace(".","_");
+		email = email + "_sig.png"
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", "/sign_s3?file_name="+email+"&file_type="+file.type);
+		xhr.onreadystatechange = () => {
         if(xhr.readyState === 4){
           if(xhr.status === 200){
             const response = JSON.parse(xhr.responseText);
