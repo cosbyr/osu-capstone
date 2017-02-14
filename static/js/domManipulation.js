b4077d66f457c8715da1257d215455017c956243
sarahecawley <cawleys@oregonstate.edu>
2017-02-14 14:17:16 -0800

    making design changes to create

--- static/js/domManipulation.js
+++ static/js/domManipulation.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 
 	$body = $("body");
 	
-	//$(document).on({
-	    //ajaxStart: function() { $body.addClass("loading");    },
-	    //ajaxComplete: function() { $body.removeClass("loading"); }    
-	//});
+	$(document).on({
+	    ajaxStart: function() { $body.addClass("loading");    },
+	   // ajaxComplete: function() { $body.removeClass("loading"); }    
+	});
 	
 	/* for find employee form on create a new award */
 	$('#get-employee-form').on('submit', function(e){
@@ -26,16 +26,19 @@ $(document).ready(function(){
 
 	  		success: function(response) {
 	  			//$("input[type=radio][name=employee-to-get-award]").remove()
+	  			$body.removeClass("loading");
+	  			$('#find-employees').removeClass('is-hidden');
 				$("input[type=radio][name=employee-to-get-award],label[for=employee-to-get-award]").remove()
 				
 				for (var i in response){
 					if(i != 'status' && i != 'message'){
 						var details = response[i].fname + " " + response[i].lname + " " +response[i].email;
 						//console.log(response[i].fname + " " + response[i].lname + " " +response[i].email);
-						$("#choose-employee").append('<label for="employee-to-get-award">' + details +'</label>' );
-						$("#choose-employee").append('<input type="radio" name="employee-to-get-award" value="'+ i +'">');
+						// $("#choose-employee").append('<label for="employee-to-get-award">' + details +'</label>' );
+						$("#choose-employee").append('<option name="employee-to-get-award" value="'+ i +'">' + details + '</option>');
 					}
 				}
+				$("#choose-employee").append('<option name="employee-to-get-award" value="'+ i +'">If desired employee is not listed please contact your admin</option>');
 				
 		    },
 
